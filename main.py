de8a2f4c2d6acd82f9d8604be7fdc411bba2e928
lucas-iribaren <lucas.iribaren@laplateforme.io>
2023-11-30 20:19:38 +0100

    ajout commentaire et modification nom variable

--- main.py
+++ main.py
@@ -1,45 +1,42 @@
 import time
-import keyboard
 
-def afficher_heure(heure):
-    heure_format = "{:02d}:{:02d}:{:02d}".format(heure[0], heure[1], heure[2])
+def afficher_heure(duree): #Comment l'heure va s'afficher
+    heure_format = "{:02d}:{:02d}:{:02d}".format(duree[0], duree[1], duree[2]) 
     print(heure_format)
 
+#Mes input
 def regler_heure():
     heures = int(input("Entrez l'heure : "))
     minutes = int(input("Entrez les minutes : "))
     secondes = int(input("Entrez les secondes : "))
-    return heures, minutes, secondes
-
-
+    return heures, minutes, secondes #Renvoi l'heure initialisée
 def regler_alarme():
     alarme_heures= int(input("Entrez l'heure de l'alarme : "))
     alarme_minutes= int(input("Entrez les minutes de votre alarme : "))
     alarme_secondes= 00
-    return alarme_heures,alarme_minutes,alarme_secondes
+    return alarme_heures,alarme_minutes,alarme_secondes #Revoi à quel heure l'alarme va sonner
 
 def main():
-    heure_a_afficher = regler_heure()
+    temps = regler_heure()
     heure_alarme = regler_alarme()
 
 
-    while True:
-        afficher_heure(heure_a_afficher)
-        time.sleep(1)
+    while True: #Boucle infini
+        afficher_heure(temps) #On insère les données de temps dans le format
+        time.sleep(1) #Temps d'actualisation
+        temps = (temps[0], temps[1], temps[2] + 1) # <-- +1 à mes secondes à chaque temps d'actualisation
 
-        heure_a_afficher = (heure_a_afficher[0], heure_a_afficher[1], heure_a_afficher[2] + 1)
+        if temps[2] == 60: #Si mes secondes sont à 60 
+            temps = (temps[0], temps[1] + 1, 0) #Alors ajoute 1 au minutes et mes secondes reviennent à 0
 
-        if heure_a_afficher[2] == 60:
-            heure_a_afficher = (heure_a_afficher[0], heure_a_afficher[1] + 1, 0)
-
-        if heure_a_afficher[1] == 60:
-            heure_a_afficher = (heure_a_afficher[0] + 1, 0, 0)
+        if temps[1] == 60:#Si mes minutes sont à 60
+            temps = (temps[0] + 1, 0, 0)#Alors ajoute 1 au heures et mes minutes et secondes reviennent à 0
         
-        if heure_a_afficher[0] == 24:
-            heure_a_afficher = (0, heure_a_afficher[1], heure_a_afficher[2])
+        if temps[0] == 24:#Si mes heures sont à 24
+            temps = (0, temps[1], temps[2])#Alors mes heures reviennent à 0
 
-        if heure_alarme == heure_a_afficher:
-            print("C'est l'heure !!!")
+        if temps == heure_alarme : #Si mon temps est égale à l'heure de mon alarme
+            print("C'est l'heure !!!") #Print
 
 
 
